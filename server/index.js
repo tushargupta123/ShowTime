948378b0e452f376499f13592f2d347a71c95ac5
tushargupta123 <tushargupta2k3@gmail.com>
2024-06-26 17:46:32 +0530

    basic server setup

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+async function main() {
+    await mongoose.connect("mongodb+srv://tushargupta2k3:tUshar%40123@twitter.fzbvq5v.mongodb.net/showtime");
+}
+main().catch((err) => console.log(err));
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(process.env.PORT,() => {
+    console.log("server listening on port " + process.env.PORT);
+})
