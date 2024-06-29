8ee243e11b41c3489a532f3c209a37cfd8734cc7
tushargupta123 <tushargupta2k3@gmail.com>
2024-06-29 11:42:38 +0530

    deployed api endpoint added

--- client/src/apicalls/index.js
+++ client/src/apicalls/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "",
+    baseURL: "https://show-time-murex.vercel.app",
     headers: {
         withCredentials: true,
         "Content-Type": "application/json",
